dab2a63fc6ff3e7fd42eb3fcac31ebaa221f8be6
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-18 21:57:31 +0300

    Add v-for to create a 30 by 30 grid

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,5 +1,7 @@
 <template>
-  <Pixel />
+  <div class="canvas">
+    <Pixel v-for="n in 30 * 30" :key="n" />
+  </div>
 </template>
 
 <script>
@@ -12,3 +14,11 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.canvas {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 900px;
+}
+</style>
