2b8215ce7d9b0fcec3ab4d0316490cb9cfb9b516
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-18 22:51:25 +0300

    Fix the app and complete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <ColorPicker :color="color" />
-    <Canvas />
+    <Canvas :pixels="pixels" />
   </div>
 </template>
 
@@ -9,11 +9,16 @@
 import Canvas from "./components/Canvas";
 import ColorPicker from "./components/ColorPicker";
 
+const defaultColor = "white";
+
 export default {
   name: "App",
   data: function() {
     return {
-      color: "white"
+      color: defaultColor,
+      pixels: Array(30 * 30)
+        .fill()
+        .map(() => defaultColor)
     };
   },
   components: {
@@ -24,18 +29,20 @@ export default {
     this.$root.$on("updatecolor", color => {
       this.color = color;
     });
+    this.$root.$on("clickedpixel", index => {
+      this.pixels.splice(index, 1, this.color);
+    });
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   background-color: #333;
   display: flex;
   justify-content: center;
--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="canvas">
-    <Pixel v-for="n in 30 * 30" :key="n" color="blue" />
+    <Pixel isInCanvas v-for="(color, index) in pixels" :color="color" :key="index" :index="index" />
   </div>
 </template>
 
@@ -11,6 +11,9 @@ export default {
   name: "Canvas",
   components: {
     Pixel
+  },
+  props: {
+    pixels: Array
   }
 };
 </script>
