14a3fd96f187098abef32cebb286f5c5ea8f4440
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-18 21:17:55 +0300

    Create template syntax for Pixel component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Pixel msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Pixel from "./components/Pixel.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Pixel
   }
 };
 </script>
---
+++ src/components/Pixel.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="pixel">hi</div>
+</template>
+
+<style scoped>
+.pixel {
+  border: 1px solid lightgray;
+}
+</style>
